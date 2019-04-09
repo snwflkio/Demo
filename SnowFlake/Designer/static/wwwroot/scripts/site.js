f28966796d75e5c75c85eef21316fe532dbbc694
Dante Pasionek <dante.pasionek1@gmail.com>
2019-04-08 18:08:17 -0600

    Added ability to change title

--- SnowFlake/Designer/static/wwwroot/scripts/site.js
+++ SnowFlake/Designer/static/wwwroot/scripts/site.js
@@ -12,8 +12,8 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
   ev.target.innerHTML = document.getElementById(data).innerHTML;
-  console.log(ev.target.innerHTML);
-  val = ev.target.innerHTML;
+  console.log("DATA: " + data)
+  document.getElementById(data + '-setter').style["display"] = "block";
 }
 
 function getInfo()
