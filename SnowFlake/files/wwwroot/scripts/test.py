dffd35b1972d0f24465858cc05f66b451a6df876
Dante Pasionek <dante.pasionek1@gmail.com>
2019-04-09 13:19:34 -0600

    Cleaning up repo

--- SnowFlake/files/wwwroot/scripts/test.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-f = open('test.html', 'w')
-
-f.write(sys.argv[1])
-f.close()
--- SnowFlake/wireframes/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('wireframes.views',
-    #url(r'^$', 'home', name="wireframes-home"),
-    url(r'^create/$', 'create', name="wireframes-create"),
-    url(r'^(?P<wireframe_id>[0-9]+)/edit/$', 'edit', name="wireframes-edit"),
-    url(r'^(?P<wireframe_id>[0-9]+)/save/$', 'save', name="wireframes-save"),
-)
