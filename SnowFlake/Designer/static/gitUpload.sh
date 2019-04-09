a462465be075c3143f2defb987a05aa5944f69a6
Walker Schmidt <schmidt.walker@yahoo.com>
2019-04-09 12:29:04 -0600

    lol

--- SnowFlake/Designer/static/gitUpload.sh
+++ SnowFlake/Designer/static/gitUpload.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 mkdir -p ../../../../snwflkio.github.io/Creations/$1;
+cd ../../../../snwflkio.github./Creations/$1;
+echo "poop" >> ho.txt
+
 git add .;
 git commit -m "$2";
 git push origin master;
